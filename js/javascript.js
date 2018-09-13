1aa9e8695430e558c91f834fd59a70104445e82e
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-09-13 21:08:47 +0200

    grid css / js core ares and  efecive date

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,12 @@ const templateOutcome = document.querySelector("#learn-outcome-template").conten
 const insertOutcome = document.querySelector(".learn-outcome");
 const templateAreas = document.querySelector("#core-areas-template").content;
 const insertAreas = document.querySelector(".core-areas");
+//const imgbase = "../images/";
+const templateEDate = document.querySelector("#template-efective-date").content;
+const insertEDate = document.querySelector(".effective-date");
+
+
+//------------LEARN OUTCOME FUNCTION
 
 window.addEventListener("load", () => {
     fetch("../json/outcome.json").then(res => res.json()).then(show)
@@ -15,6 +21,9 @@ function show(data) {
     insertOutcome.appendChild(clone)
 };
 
+
+//------------CORE AREAS FUNCTION
+
 window.addEventListener("load", () => {
     fetch("../json/coreareas.json").then(res => res.json()).then(showAreas)
 });
@@ -27,8 +36,26 @@ function showAreas(areas) {
 function showSingleLeadArticle(article) {
     console.log(article)
     const cloneAreas = templateAreas.cloneNode(true);
+    //cloneAreas.querySelector(".core-areas-img").src = "../images/" + article.image + ".png";
     cloneAreas.querySelector(".core-areas-h2").textContent = article.name;
     cloneAreas.querySelector(".slogan").textContent = article.slogan;
     cloneAreas.querySelector(".outcome-areas").textContent = article.outcome;
     insertAreas.appendChild(cloneAreas)
 }
+
+
+//------------EFECTIVE DATE FUNCTION
+
+window.addEventListener("load", () => {
+    fetch("../json/date.json").then(res => res.json()).then(showDate)
+});
+
+
+function showDate(info) {
+    console.log(info);
+    const cloneDate = templateEDate.cloneNode(true);
+    cloneDate.querySelector(".edate-h2").textContent = info.titleDate;
+    cloneDate.querySelector(".edate-p").textContent = info.descriptionDate;
+    insertEDate.appendChild(cloneDate)
+};
+
