3c73fbc846b56aa7a9afdb01338b711bb1f8e3b0
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-09-13 19:53:57 +0200

    areas fetch js

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,34 @@
 const templateOutcome = document.querySelector("#learn-outcome-template").content;
 const insertOutcome = document.querySelector(".learn-outcome");
 const templateAreas = document.querySelector("#core-areas-template").content;
+const insertAreas = document.querySelector(".core-areas");
 
-window.addEventListener("load", ()=>{
-  fetch("../json/outcome.json").then(res=>res.json()).then(show)
+window.addEventListener("load", () => {
+    fetch("../json/outcome.json").then(res => res.json()).then(show)
 });
 
-function show(data){
+function show(data) {
     console.log(data);
     const clone = templateOutcome.cloneNode(true);
     clone.querySelector(".outcome-h2").textContent = data.title;
     clone.querySelector(".outcome-p").textContent = data.description;
     insertOutcome.appendChild(clone)
+};
+
+window.addEventListener("load", () => {
+    fetch("../json/coreareas.json").then(res => res.json()).then(showAreas)
+});
+
+function showAreas(areas) {
+    console.log(areas);
+    areas.article.forEach(showSingleLeadArticle)
+};
+
+function showSingleLeadArticle(article) {
+    console.log(article)
+    const cloneAreas = templateAreas.cloneNode(true);
+    cloneAreas.querySelector(".core-areas-h2").textContent = article.name;
+    cloneAreas.querySelector(".slogan").textContent = article.slogan;
+    cloneAreas.querySelector(".outcome-areas").textContent = article.outcome;
+    insertAreas.appendChild(cloneAreas)
 }
