b04b25a1faaaf75b520cf392185bd8c228dfdfef
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-09-13 18:42:11 +0200

    java scrpt start html json change

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+const templateOutcome = document.querySelector("#learn-outcome-template").content;
+const insertOutcome = document.querySelector(".learn-outcome");
+
+window.addEventListener("load", ()=>{
+  fetch("../json/outcome.json").then(res=>res.json()).then(show)
+});
+
+function show(data){
+    console.log(data);
+    const clone = templateOutcome.cloneNode(true);
+    clone.querySelector(".outcome-h2").textContent = data.title;
+    clone.querySelector(".outcome-h2").textContent = data.description;
+    insertOutcome.appendChild(clone)
+}
