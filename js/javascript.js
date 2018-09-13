df0c7849f5e5d4702148479e2fdac6161083ea7e
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-09-13 21:45:06 +0200

    js intern

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,19 @@
 const templateOutcome = document.querySelector("#learn-outcome-template").content;
 const insertOutcome = document.querySelector(".learn-outcome");
+
 const templateAreas = document.querySelector("#core-areas-template").content;
 const insertAreas = document.querySelector(".core-areas");
 //const imgbase = "../images/";
+
 const templateEDate = document.querySelector("#template-efective-date").content;
 const insertEDate = document.querySelector(".effective-date");
+
+const templateSemesters = document.querySelector("#semesters-template").content;
+const insertSemester = document.querySelector(".semesters");
+
+const templateInternship = document.querySelector("#intern-template").content;
+const insertInternship = document.querySelector(".intership");
+
 
 
 //------------LEARN OUTCOME FUNCTION
@@ -30,10 +39,10 @@ window.addEventListener("load", () => {
 
 function showAreas(areas) {
     console.log(areas);
-    areas.article.forEach(showSingleLeadArticle)
+    areas.article.forEach(showSingleArticle)
 };
 
-function showSingleLeadArticle(article) {
+function showSingleArticle(article) {
     console.log(article)
     const cloneAreas = templateAreas.cloneNode(true);
     //cloneAreas.querySelector(".core-areas-img").src = "../images/" + article.image + ".png";
@@ -59,3 +68,36 @@ function showDate(info) {
     insertEDate.appendChild(cloneDate)
 };
 
+//------------SEMESTERS FUNCTION
+
+/*window.addEventListener("load", () => {
+    fetch("../json/semesters.json").then(res => res.json()).then(showSemesters)
+});
+
+function showSemesters(semester) {
+    console.log(semester);
+};*/
+
+/*function showSingleLeadArticle(article) {
+    console.log(article)
+    const cloneAreas = templateAreas.cloneNode(true);
+    cloneAreas.querySelector(".core-areas-h2").textContent = article.name;
+    cloneAreas.querySelector(".slogan").textContent = article.slogan;
+    cloneAreas.querySelector(".outcome-areas").textContent = article.outcome;
+    insertAreas.appendChild(cloneAreas)
+}*/
+
+//------------INTERNSHIP FUNCTION
+
+window.addEventListener("load", () => {
+    fetch("../json/intern.json").then(res => res.json()).then(showIntern)
+});
+
+function showIntern(intern) {
+    console.log(intern);
+    const cloneintern = templateInternship.cloneNode(true);
+    cloneintern.querySelector(".edate-h2").textContent = info.titleDate;
+    cloneintern.querySelector(".edate-p").textContent = info.descriptionDate;
+    insertEDate.appendChild(cloneintern)
+};
+}
